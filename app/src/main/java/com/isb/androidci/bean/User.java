591db1d0ef0aae20636ee4d8f6f32ce54cd8f213
unknown <amegia@gmail.com>
2015-06-04 23:15:38 +0200

    Subida inicial

---
+++ app/src/main/java/com/isb/androidci/bean/User.java
@@ -0,0 +1,13 @@
+package com.isb.androidci.bean;
+
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
